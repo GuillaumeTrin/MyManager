09a5c57fbec2ae254579fb3baf5927cc855a12cc
Laure Beaucoud <laure.beaucoud@gmail.com>
2021-06-01 12:34:33 +0200

    model + controller + routes OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
-  get 'albums/index'
-  get 'albums/show'
-  get 'albums/new'
-  get 'albums/create'
-  devise_for :users
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
 end
