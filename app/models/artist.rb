f23f29ab2fce81d9af49ef5e5f5f5a8940580b01
Emlyne Charbonnier <emlyne.charbonnier@gmail.com>
2021-06-01 14:19:48 +0200

    validation models

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ApplicationRecord
-  belongs_to :user,
-  has_many :albums,
-  has_many :posts,
+  belongs_to :user
+  has_many :albums
+  has_many :posts
   validates :name, presence: true, uniqueness: true
+  validates :id_facebook, presence: true, uniqueness: true
 end
