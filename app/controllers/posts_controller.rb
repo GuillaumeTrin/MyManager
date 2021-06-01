def6d427d4d157fb7ae14ee2717172ad4d0ffefe
Emlyne Charbonnier <emlyne.charbonnier@gmail.com>
2021-06-01 12:16:06 +0200

    created post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,43 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save!
+      redirect_to artist_path, notice: "You've just created a new post"
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @post.update(post_params)
+    redirect_to artist_path
+  end
+
+  def delete
+    @post = Post.destroy
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, :image, :published, :published_at)
+  end
+end
