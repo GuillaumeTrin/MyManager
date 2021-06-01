208d03ceb3b60ca9f5c0aafd8cb8ac9a389d7148
Emlyne Charbonnier <emlyne.charbonnier@gmail.com>
2021-06-01 17:58:37 +0200

    calendar dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    start_date = params.fetch(:start_date, Date.current).to_date
+    @posts = Post.where(published_at: start_date..Date.current)
   end
 end
