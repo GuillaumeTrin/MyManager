a4b571c18ca81fd68779ebb99efe4cffda32731c
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-02 12:41:10 +0200

    background jobs

--- app/jobs/post_to_facebook_job.rb
+++ app/jobs/post_to_facebook_job.rb
@@ -1,9 +1,9 @@
 class PostToFacebookJob < ApplicationJob
   queue_as :default
 
-  def perform(artist, content)
+  def perform(post)
     client = OAuth2::Client.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], site: 'https://graph.facebook.com', token_url: "/oauth/access_token")
-    token = OAuth2::AccessToken.new(client,artist.facebook_access_token)
-    token.post("#{artist.id_facebook}/feed", params: { message: content})
+    token = OAuth2::AccessToken.new(client,post.artist.facebook_access_token)
+    token.post("#{post.artist.id_facebook}/feed", params: { message: post.content})
   end
 end
