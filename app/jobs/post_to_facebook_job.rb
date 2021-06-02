31a1d4536ee462e2f3b7935fff51c6578df18176
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-02 11:29:39 +0200

    facebook a partir d un job synchrone

---
+++ app/jobs/post_to_facebook_job.rb
@@ -0,0 +1,9 @@
+class PostToFacebookJob < ApplicationJob
+  queue_as :default
+
+  def perform(artist, content)
+    client = OAuth2::Client.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], site: 'https://graph.facebook.com', token_url: "/oauth/access_token")
+    token = OAuth2::AccessToken.new(client,artist.facebook_access_token)
+    token.post("#{artist.id_facebook}/feed", params: { message: content})
+  end
+end
