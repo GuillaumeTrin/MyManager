d7904f9eb5a04690ce87630550aa0ad559a4b3ec
GuillaumeTrin <trin.guillaume@gmail.com>
2021-06-02 12:14:49 +0200

    seed n1 Ok et valide

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+Post.destroy_all
+Album.destroy_all
+Artist.destroy_all
+User.destroy_all
+
+
+user_1 = User.create!(first_name: "Guillaume", last_name: "Trin", email: "guillaume.trin@gmail.com", password: "totototo")
+
+artist_ephemeris = Artist.create!(name: "Nicolas", id_facebook: "1234", user: user_1)
+album_moobbow = Album.create!(name: "Moonbow", out_at: "2021-06-15", artist: artist_ephemeris)
+post_announce_album = Post.create!(title: "Announce Album", content: "Our next on Sahman Records, is an impressive collaboration between our home artist Ephemeris and the talented producer Waveorm from Grece. This epic trancer will float you gently to a pure trippy oasis ! Available the 12.10.20 !", artist: artist_ephemeris, album: album_moobbow)
+
