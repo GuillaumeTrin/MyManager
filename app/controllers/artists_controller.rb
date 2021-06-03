738bc5b089ad0d5a38909f16d2b943dadfd408b6
Emlyne Charbonnier <emlyne.charbonnier@gmail.com>
2021-06-03 12:25:51 +0200

    finalized all calendar views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,6 +3,7 @@ class ArtistsController < ApplicationController
     @artist = Artist.find(params[:id])
     start_date = params.fetch(:start_date, Date.current).to_date
     @posts = Post.where(published_at: start_date..Date.current, artist: @artist)
+    @today_posts = Post.where(published_at: Date.today..Date.today + 1.days, artist: @artist)
   end
 
   def index
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,7 +2,7 @@ class Artist < ApplicationRecord
   belongs_to :user
   has_many :albums
   has_one_attached :picture
-  has_many :posts
+  has_many :posts, dependent: :destroy
   validates :name, presence: true, uniqueness: true
   validates :id_facebook, presence: true, uniqueness: true
 end
