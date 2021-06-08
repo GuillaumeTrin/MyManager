e41e58388d745e521f2bd41f741129c6ddc49a88
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-08 17:27:45 +0200

    stats par artistes avec rgb

--- app/javascript/plugins/init_global_chart.js
+++ app/javascript/plugins/init_global_chart.js
@@ -4,6 +4,7 @@ const initGlobalChart = () => {
     const ctx = document.getElementById('myGlobalChart');
     if (!ctx)
     return
+    const data_global = JSON.parse(ctx.dataset.stats);
     const artists_ids = JSON.parse(ctx.dataset.artists);
     let artist_datasets = []
     artists_ids.forEach((artist_id) => { 
@@ -32,13 +33,18 @@ const initGlobalChart = () => {
 
 }
 
+const random_rgba = () => {
+    const o = Math.round, r = Math.random, s = 255;
+    return 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+}
+
 
 const build_dataset = (json_stat) => {
-   
+    const rgb = random_rgba();
     return {
         label: json_stat["name"],
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
+        backgroundColor: rgb,
+        borderColor: rgb,
         data: json_stat["stats"],
         // data:  [{x:'2016-12-25', y:20}, {x:'2016-12-26', y:10}],
       }
