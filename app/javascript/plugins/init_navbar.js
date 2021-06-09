f3e044bf74bfff3f0eb9b71078fdbb8923a21ed8
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-09 17:13:56 +0200

    si n existe pas return

--- app/javascript/plugins/init_navbar.js
+++ app/javascript/plugins/init_navbar.js
@@ -1,7 +1,8 @@
 const initNavbar = () => {
   const menu = document.querySelector(".nav-icon");
   const sidebar = document.querySelector(".sidebar");
-
+  if (!menu)
+    return
   menu.addEventListener("click", (event) => {
     sidebar.classList.toggle("sidebar-default");
   });
