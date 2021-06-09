fde497b8cf9c85b937583b92ef828de305c8f12c
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-09 11:26:36 +0200

    update artist in later job

--- app/services/update_all_data_artists.rb
+++ app/services/update_all_data_artists.rb
@@ -2,7 +2,7 @@ class UpdateAllDataArtists < BaseService
   def call
     artists = Artist.all
     artists.find_each do |artist|
-      UpdateOneArtistJob.perform_now(artist)
+      UpdateOneArtistJob.perform_later(artist)
     end
   end
 end
