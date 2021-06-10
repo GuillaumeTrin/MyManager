48b9a353b01bacfbcb983678be313ae331e5159d
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-10 18:04:03 +0200

    reschedule post_fix

--- app/jobs/reschedule_facebook_job.rb
+++ app/jobs/reschedule_facebook_job.rb
@@ -4,7 +4,6 @@ class RescheduleFacebookJob < ApplicationJob
   def perform(post)
     # Do something later
     job = Sidekiq::ScheduledSet.new.find_job(post.job_id)
-    job&.reschedule(post.published_at)
-    
+    job.reschedule(post.published_at - 2.hours)
   end
 end
